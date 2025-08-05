f8493b28f8f3e1cc169b19acacb66e5f26d6577a
Bil Bas <bil.bagpuss@gmail.com>
2025-08-05 20:06:23 +0100

    Added colour constants . rearranged status bars.

---
+++ scripts/colours.ts
@@ -0,0 +1,18 @@
+namespace Colour {
+    export const TRANS = 0
+    export const WHITE = 1
+    export const RED = 2
+    export const PINK = 3
+    export const ORANGE = 4
+    export const YELLOW = 5
+    export const TEAL = 6
+    export const GREEN = 7
+    export const BLUE = 8
+    export const LIGHT_BLUE = 9
+    export const PURPLE = 10
+    export const LIGHT_PURPLE = 11
+    export const DARK_PURPLE = 12
+    export const TAN = 13
+    export const BROWN = 14
+    export const BLACK = 15
+}
