90c4d753d0bc592068416f3a9aca1ac0ae6e4e4f
Bil Bas <bil.bagpuss@gmail.com>
2025-08-09 22:19:33 +0100

    fixing shop

--- scripts/persons.ts
+++ scripts/persons.ts
@@ -15,17 +15,28 @@ class Person {
 }      
 
 class Shopkeeper extends Person {
+    _present: boolean
+
     get image(): Image { return sprites.builtin.villager1WalkFront1 }
 
-    _label(text: string, value: number) {
+    constructor(tile: tiles.Location) {
+        super(tile)
+        this._present = true
+    }
+
+    _label(text: string, value?: number) {
         let padding = 20 - text.length
-        return `${text}${padStart(value.toString(), padding)}GC`
+        return `${text}${padStart(value ? (value.toString() + "GC") : "", padding)}`
     }
 
     touch() {
+        if (!this._present) return
+
         let forSale: Array<[string, Image]> = []
+        forSale.push([this._label("Leave without buying"), sprites.projectile.bubble4])
         forSale.push([this._label("Life Potion", 100), sprites.projectile.heart3])
         forSale.push([this._label("Mana Crystal", 100), sprites.projectile.star3])
+        forSale.push([this._label("Skeleton Key", 100), assets.image`key`])
 
         for (let spell of SPELL_BOOK)  {
             forSale.push([this._label(`${spell.mana || '*'} ${spell.title}`, spell.value), spell.icon])
@@ -36,22 +47,42 @@ class Shopkeeper extends Person {
             forSale,
             (selectedIndex: number) => {
                 if (selectedIndex == 0) {
-                    player.life += 1
+                    // just leave
                 } else if (selectedIndex == 1) {
-                    player.mana += 1
+                    if (player.coins >= 100) {
+                        player.coins -= 100
+                        player.life += 1
+                    } else {
+                        return
+                    }
+                } else if (selectedIndex == 2) {
+                    if (player.coins >= 100) {
+                        player.coins -= 100
+                        player.mana += 1
+                    } else {
+                        return
+                    }
+                } else if (selectedIndex == 3) {
+                    if (player.coins >= 100) {
+                        player.coins -= 100
+                        player.keys += 1
+                    } else {
+                        return
+                    }
                 } else {
-                    let spell: Spell = SPELL_BOOK[selectedIndex - 2]
+                    let spell: Spell = SPELL_BOOK[selectedIndex - 4]
 
                     if (player.coins >= spell.value) {
                         player.coins -= spell.value
                         player.secondarySpell = spell
-                        console.log(`You chose ${spell.title}`)
-                        this._sprite.destroy()
-                        menu.close()
                     } else {
-                        console.log(`You can't afford ${spell.title}`)
+                        return
                     }
                 }
+
+                menu.close()
+                this._present = false
+                this._sprite.destroy(effects.bubbles, 1000)
             }
         )
     }
